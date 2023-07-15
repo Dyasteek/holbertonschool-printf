5daf21c3d4c6c3ddb1fad639263704597dcbd092
jorge871 <jg303114@gmail.com>
2023-07-14 19:43:18 -0700

    Basic header file.

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _PRINTF
+#define _PRINTF
+
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+#endif
