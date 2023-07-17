37bcdd6d60b5269310362d9ec073fdce72f04c6e
Dyasteek-GTW <lawarsenik@gmail.com>
2023-07-17 05:30:32 -0700

    Added header with resourses

--- main.h
+++ main.h
@@ -1,14 +1,32 @@
 #ifndef _PRINTF
 #define _PRINTF
 
+/**libraries*/
 #include <stdio.h>
-#include <limits.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
-#include <stddef.h>
 #include <unistd.h>
+#include <stddef.h>
+#include <limits.h>
+#include <stdarg.h>
 
+/**STRUCTURE*/
+/**
+ * struct func - Structure of the format given to printf
+ * @mod: The format specifier given
+ * @f: The function that will be called
+ */
+typedef struct func
+{
+	char mod;
+	int (*f)(va_list mf);
+} func_c;
+
+/**Prototypes*/
 int _printf(const char *format, ...);
+int print_percent(va_list mf);
+int print_char(va_list mf);
+int print_string(va_list mf);
+int print_integer(va_list mf);
 
 #endif
