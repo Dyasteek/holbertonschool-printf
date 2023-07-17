f2261d2598ce0f3fdc0c81fc5d12f5021852d1d5
Dyasteek-GTW <lawarsenik@gmail.com>
2023-07-17 13:03:55 -0700

    Making our code betty friendly

--- _printf.c
+++ _printf.c
@@ -10,19 +10,12 @@ int _printf(const char *format, ...)
 	va_list val;
 	int i = 0, m = 0, cont = 0;
 	func_c func[] = {
-		{'%', print_percent},
-		{'c', print_char},
-		{'s', print_string},
-		{'d', print_integer},
-		{'i', print_integer},
-		{'\0', NULL}
-	};
+		{'%', print_percent}, {'c', print_char}, {'s', print_string},
+		{'d', print_integer}, {'i', print_integer}, {'\0', NULL}};
 
 	va_start(val, format);
 	if (format == NULL)
-	{
 		return (-1);
-	}
 	while (format[i])
 	{
 		if (format[i] == '%')
@@ -41,11 +34,6 @@ int _printf(const char *format, ...)
 					m++;
 				}
 				i++;
-			}
-			else
-			{
-				va_end(val);
-				return (-1);
 			}
 		}
 		else
