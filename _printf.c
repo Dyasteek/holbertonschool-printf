f22a7644db5cf77e42fa642760b20f472a7081ff
Dyasteek-GTW <lawarsenik@gmail.com>
2023-07-19 05:36:15 -0700

    Deleted lines and some spaces are reduced for better format

--- _printf.c
+++ _printf.c
@@ -9,15 +9,13 @@ int _printf(const char *format, ...)
 {
 	va_list val;
 	int i = 0, m = 0, cont = 0;
-	func_t func[] = {{'%', print_percent}, {'c', print_char}, {'s', print_string},
-	{'d', print_integer}, {'i', print_integer}, {'\0', NULL}
-	};
+	func_t func[] = {{'%', print_percent}, {'c', print_char},
+	{'s', print_string}, {'d', print_integer}, {'i', print_integer},
+	{'\0', NULL}};
 
 	va_start(val, format);
 	if (format == NULL)
-	{
 		return (-1);
-	}
 	while (format[i])
 	{
 		if (format[i] == '%')
@@ -32,18 +30,14 @@ int _printf(const char *format, ...)
 					{
 						cont += func[m].f(val);
 						break;
-					}
-					m++;
-				}
-				i++;
-			}
-			else
+					} m++;
+				} i++;
+			} else
 			{
 				va_end(val);
 				return (-1);
 			}
-		}
-		else
+		} else
 		{
 			write(1, &format[i], 1);
 			cont++;
@@ -53,4 +47,3 @@ int _printf(const char *format, ...)
 	va_end(val);
 	return (cont);
 }
-
